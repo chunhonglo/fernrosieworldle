300d06d09f2dbf753ff9436d61d440a588be0058
chunhonglo <anferneelo115@gmail.com>
2022-08-30 01:00:23 +0800

    configure react app for deployment to github pages

---
+++ src/components/Worldle.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Worldle() {
+  return (
+    <span className="font-bold">
+      Fern<span className="text-green-600">&</span>Rosie-dle
+    </span>
+  );
+}
---
+++ src/domain/guess.ts
@@ -0,0 +1,24 @@
+import { Direction } from "./geography";
+
+export interface Guess {
+  name: string;
+  distance: number;
+  direction: Direction;
+}
+
+export function loadAllGuesses(): Record<string, Guess[]> {
+  const storedGuesses = localStorage.getItem("guesses");
+  // return storedGuesses != null ? JSON.parse(storedGuesses) : {};
+  return {}
+}
+
+export function saveGuesses(dayString: string, guesses: Guess[]): void {
+  const allGuesses = loadAllGuesses();
+  localStorage.setItem(
+    "guesses",
+    JSON.stringify({
+      ...allGuesses,
+      [dayString]: guesses,
+    })
+  );
+}
